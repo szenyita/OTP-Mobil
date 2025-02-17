e2a4a23a459756d396d65aef53482bb7d3ddd462
szenyitamark <szenyita.mark@outlook.com>
2025-02-17 04:18:17 +0100

    db setup

--- server.js
+++ server.js
@@ -1,4 +1,12 @@
+require("dotenv").config();
+
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+
+mongoose.connect(process.env.DB_URL);
+const db = mongoose.connection;
+db.on("error", (error) => console.error(error));
+db.once("open", () => console.log("Connected to Vehicle db"));
 
 app.listen(3000, () => console.log("Server started"));
